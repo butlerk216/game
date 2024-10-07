073bbeabd650ea5978f63112de540e39ef497e45
butlerk216 <kbutler80455@uat.edu>
2024-10-06 20:18:55 -0400

    testing

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-document.getElementById("btn").addEventListener("click", Function)
-    var diceContainer = document.createElement("btn")
-    dice.src = "rollingdice.gif";
-    document.getElementById("diceContainer").appendChild(dice);
-
-    var number = (Math.random() * 10) +1;
+document.getElementById("btn").addEventListener("click",function(){
+    var number = (Math.random() * 10) + 1;
     document.getElementById("result").innerHTML = number;
+
+})
